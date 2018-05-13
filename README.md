ff3ab54c51dd8eb203190c83532f36e3837908d2
k-saiki <koh.saiki@gmail.com>
2018-05-14 01:16:19 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # mfa
 Generate TOTP(Time-based One-time Password) token with CLI.
+
+## Usage
+### Configuration
+Create config at `$HOME/.mfa/secrets`.
+```toml
+[[service]]
+Name = "amazon"
+secret = "your secret key"
+
+[[service]]
+Name = "google"
+secret = "your secret key"
+
+[[service]]
+Name = "github"
+secret = "your secret key"
+```
+
+### Generate token
+```bash
+$ mfa gen amazon
+999999
+```
+
+### List services
+```bash
+$ mfa list
+amazon
+google
+github
+```
+
+## Install
+```bash
+go get github.com/k-saiki/mfa
+```
---
+++ mfa.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	"github.com/mitchellh/go-homedir"
+	"github.com/pquerna/otp/totp"
+	"github.com/urfave/cli"
+)
+
+const configDefault = ".mfa/secrets"
+
+type Config struct {
+	Service []MFAConfig `toml:"service"`
+}
+
+type MFAConfig struct {
+	Name   string `toml:"name"`
+	Secret string `toml:"secret"`
+}
+
+func main() {
+	var config Config
+	if err := loadConfig(configDefault, &config); err != nil {
+		log.Fatal(err)
+	}
+
+	app := cli.NewApp()
+	app.EnableBashCompletion = true
+	app.Name = "mfa"
+	app.Usage = "Generate TOTP(Time-based One-time Password) token."
+	app.Version = "0.0.1"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "list",
+			Usage: "List configured services.",
+			Action: func(context *cli.Context) error {
+				for _, cfg := range config.Service {
+					fmt.Printf("%s\n", cfg.Name)
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "gen",
+			Usage: "Generate TOTP token.",
+			Action: func(context *cli.Context) error {
+				now := time.Now()
+				service := context.Args().Get(0)
+				for _, cfg := range config.Service {
+					if cfg.Name == service {
+						token, _ := totp.GenerateCode(cfg.Secret, now)
+						fmt.Println(token)
+						break
+					}
+					msg := service + " does not exist."
+					return errors.New(msg)
+				}
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func loadConfig(configPath string, config *Config) error {
+	homeDir, err := homedir.Dir()
+	if err != nil {
+		return err
+	}
+
+	configFullPath := filepath.Join(homeDir, configPath)
+
+	if _, err := toml.DecodeFile(configFullPath, config); err != nil {
+		return err
+	}
+
+	return nil
+}
