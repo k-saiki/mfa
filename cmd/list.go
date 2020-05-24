89f275b9b3ab75cb469137bc223bff841b19b057
saiki.ko <saiki.ko@classmethod.jp>
2020-05-25 00:01:27 +0900

    use cobra

--- cmd/list.go
+++ cmd/list.go
@@ -9,7 +9,7 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "Read config file and print services",
+	Short: "Print services",
 
 	Run: func(cmd *cobra.Command, args []string) {
 		for _, s := range config.Service {
@@ -20,14 +20,4 @@ var listCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(listCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
